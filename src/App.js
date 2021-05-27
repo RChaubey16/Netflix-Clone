0085415d78f9b2b3fbd4de82324f33d8b5036e54
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-05-27 22:47:05 +0530

    Create the Row component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './App.css';
 import Row from './Row';
+import requests from './requests'
 
 function App() {
   return (
     <div className="App">
       <h1>Hey Raj! lets build Netflix Clone</h1>
       {/* Calling the Row component in App.js */}
-      <Row title="NETFLIX ORIGINALS"/>
-      <Row title="Trending Now"/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,13 +1,53 @@
 // Row Component
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from './axios';
+// import requests from './requests';
+
+const baseurl = 'https://image.tmdb.org/t/p/original/';
 
 // getting the title from the Row title mentioned in the App.js Row component call
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
+
+    // Using State  
+    const [movies, setMovies] = useState([]);
+
+    // A snippet of code which runs based on a specific condition/variable
+    // By using this Hook, you tell React that your component needs to do something after render
+    useEffect(() => { 
+        // if [], run once when the row loads and dont run again
+
+        async function fetchData() {
+            // Here we are making requests to get the info
+
+            // request will have the data fetched frome the API and we can use it to display the movies
+            const request = await axios.get(fetchUrl);
+
+            // here we are just checking what data is coming back from the requested URL
+            console.log(request.data.results);
+            setMovies(request.data.results);
+            return request; 
+        }
+        fetchData();
+
+    }, [fetchUrl]);
+    // you have to mention the fetchUrl as it is out of the useEffect block and is one the dependencies of useEffect Hook
+
+    console.table(movies);  
+
   return (
-    <div>
+    <div className="row"> 
         {/* title */}
         <h2>{title}</h2>
+
+        <div className="row__posters">
+            {/* several row__poster(s) */}
+            
+            {/* movies.map will iterate through the array conataining the netflix movies */}
+            {movies.map(movie => (
+                <img src={`${baseurl}${movie.poster_path}` } alt={movie.name}></img>
+            ))}
+        </div>
 
         {/* container -> posters */}
 
